f6ee9590586fd52dfbc3e88a64a2b524e8dfbec5
Srdjan Bogojevic <srdjan.bogojevic@gmail.com>
2020-01-29 16:08:51 +0100

    test

---
+++ AutoMapperTest.Test/Model/TestModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoMapperTest.Test.Model
+{
+    internal class TestModel
+    {
+        public TestModel(string name, string surname);
+
+        public string Name { get; }
+        public string SurName { get; }
+    }
+}
